1ac69546694217efc5beb8ceaacc90e154cbe99d
Александр <sivko8898@gmai.com>
2023-05-20 08:50:20 +0300

    origin/main

---
+++ dist/js/all.js
@@ -0,0 +1,1 @@
+const burger=document.querySelector(".burger"),nav=document.querySelector(".section-nav");burger.addEventListener("click",(function(){this.classList.toggle("active"),nav.classList.toggle("open")}));
